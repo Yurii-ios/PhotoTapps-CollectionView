3770ef0ea1c847865b9bae5c0c7d9cda146400a5
Yurii <Yurii>
2020-04-06 23:36:15 +0200

    CollectionView Flow Layout.

--- PhotoTapps-CollectionView/PhotosCollectionVC.swift
+++ PhotoTapps-CollectionView/PhotosCollectionVC.swift
@@ -42,5 +42,30 @@ class PhotosCollectionVC: UICollectionViewController  {
 }
 // pozwoliaet konfigyrirowat nastrojki autolayout w kode!
 extension PhotosCollectionVC: UICollectionViewDelegateFlowLayout {
+    // nastraiwaem wid ja4eek
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // dwe ja4ejki na idin riad
+        let itemsPerRow: CGFloat = 2
+       // koli4estwo otstypow w 1 riady
+        let paddingWidth = 20 * (itemsPerRow + 1)
+        // wuzwa4aem kakaja shirina nam dostypna dlia razmes4enija ja4eek
+        let availableWeidth = collectionView.frame.width - paddingWidth
+        // wus4ituwaem shiriny konkretnogo obekta
+        let widthPerIten = availableWeidth / itemsPerRow
+        // razmwr ja4eek
+        return CGSize(width: widthPerIten, height: widthPerIten)
+    }
     
+    // nastraiwaem granicu ja4eek do kraew ekrana i meždy soboj, eto otstypu ne dlia CollectionView a dlia samix section!!
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+    }
+    // nastrojka rasstojanija meždy linijami section
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return CGFloat(20)
+    }
+    // nastrojka rasstojanija meždy samimi obektami
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return CGFloat(20)
+    }
 }
