95639ddaea1742b7c6720f2bc9f8627713000bd0
Yurii <Yurii>
2020-04-07 16:10:37 +0200

    Flow Layout Advanced Setup

--- PhotoTapps-CollectionView/PhotosCollectionVC.swift
+++ PhotoTapps-CollectionView/PhotosCollectionVC.swift
@@ -9,11 +9,26 @@
 import UIKit
 
 class PhotosCollectionVC: UICollectionViewController  {
-
+    // dwe ja4ejki na odin riad
+          let itemsPerRow: CGFloat = 2
+    // delaem otstypu ot sekcij
+    let sectionInserts = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    
+// drygaja nastrojka layouta bez ispolzowanija UICollectionViewDelegateFlowLayout
+    //        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout // poly4aem dostyp po nastrojke layout
+    //        // nastraiwaem razmer ja4eek
+    //        layout.itemSize = CGSize(width: 70, height: 30)
+    //        // nastrojka rasstojanija meždy otstypami ja4eek
+    //        layout.sectionInset =  UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+    //        // rastojanie meždy obektami po strokam
+    //        layout.minimumLineSpacing = CGFloat(1)
+    //        // rastajanija meždy samimi obektami
+    //        layout.minimumInteritemSpacing = CGFloat(1)
+    //        layout.scrollDirection = .vertical
+    //        // yberaem polosky sostojanija prokrytki
+    //        collectionView.showsVerticalScrollIndicator = false
     }
 
     // MARK: UICollectionViewDataSource
@@ -44,10 +59,9 @@ class PhotosCollectionVC: UICollectionViewController  {
 extension PhotosCollectionVC: UICollectionViewDelegateFlowLayout {
     // nastraiwaem wid ja4eek
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        // dwe ja4ejki na idin riad
-        let itemsPerRow: CGFloat = 2
+      
        // koli4estwo otstypow w 1 riady
-        let paddingWidth = 20 * (itemsPerRow + 1)
+        let paddingWidth = sectionInserts.left * (itemsPerRow + 1) // .left - chranit konstanty 20, kotoraja zadana wushe
         // wuzwa4aem kakaja shirina nam dostypna dlia razmes4enija ja4eek
         let availableWeidth = collectionView.frame.width - paddingWidth
         // wus4ituwaem shiriny konkretnogo obekta
@@ -55,17 +69,17 @@ extension PhotosCollectionVC: UICollectionViewDelegateFlowLayout {
         // razmwr ja4eek
         return CGSize(width: widthPerIten, height: widthPerIten)
     }
-    
+
     // nastraiwaem granicu ja4eek do kraew ekrana i meždy soboj, eto otstypu ne dlia CollectionView a dlia samix section!!
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        return sectionInserts
     }
     // nastrojka rasstojanija meždy linijami section
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return CGFloat(20)
+        return sectionInserts.left
     }
     // nastrojka rasstojanija meždy samimi obektami
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return CGFloat(20)
+        return sectionInserts.left
     }
 }
